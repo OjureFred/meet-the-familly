e45c460140a41c920750dcd4dbd4ac629bd47739
Fredrick Ojure <fredojure@gmail.com>
2022-10-30 15:17:47 +0300

    Completed unit test, integration test and code for geektrust translate method

--- family_tree/geektrust.py
+++ family_tree/geektrust.py
@@ -3,8 +3,7 @@ class FamilyTree:
     def __init__(self):
         self.family_tree = {}
     
-
-    
+      
     def add_child(self):
         return 'CHILD_ADDITION_SUCCEEDED'
     
@@ -20,9 +19,57 @@ class Geektrust:
     def __init__(self):
         self.family_tree = FamilyTree()
     
+    def construct_add_child_method_call(self, *args):
+        return 'self.family_tree.add_child("{}", "{}", "{}")'.format(
+            args[1],
+            args[2],
+            args[0]
+        )
+    
+    def construct_add_spouse_method_call(self, *args):
+         return 'self.family_tree.add_spouse("{}", "{}", "{}")'.format(
+            args[1],
+            args[2],
+            args[0]
+        )
+    
+    def construct_get_relationship_method_call(self, *args):
+        switch_relationship = {
+            'Paternal_Aunt': 'paternal_aunt',
+            'Paternal_Uncle': 'paternal_uncle',
+            'Maternal_aunt': 'maternal_aunt',
+            'Maternal_Uncle': 'maternal_uncle',
+            'Brother_in_law': 'brother_in_law',
+            'Sister_in_law': 'sister_in_law',
+            'Son': 'son',
+            'Daughter': 'daughter',
+            'Siblings': 'siblings'
+        }
+        
+        relationship_type = switch_relationship.get(args[1], None)
+        if not relationship_type:
+            return None
+        return 'self.family_tree.get_relationship("{}", "{}")'.format(
+            args[0],
+            args[1]
+            
+        )
+    
+    
     def translate(self, filename):
+        switch_construct_method = {
+            'ADD_CHILD': self.construct_add_child_method_call,
+            'ADD_SPOUSE': self.construct_add_spouse_method_call,
+            'GET_RELATIONSHIP': self.construct_get_relationship_method_call
+        }
         with open(filename, 'r') as fr:
             instructions = fr.readlines()
+            for instruction in instructions:
+                tokens = instruction.split(" ")
+                construct_method = switch_construct_method.get(tokens[0], None)
+                if not construct_method:
+                    continue
+                construct_method(*tuple(tokens[1:]))
 
 
 if __name__ == '__main__':
--- tests/integration/test_geektrust.py
+++ tests/integration/test_geektrust.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 from unittest.mock import patch
-#from geektrust import Geektrust
 from family_tree.geektrust import Geektrust
 
 class TestGeekTrust(TestCase):
@@ -8,15 +7,14 @@ class TestGeekTrust(TestCase):
     def setUp(self):
         self.geektrust_app = Geektrust()
     
-    @patch('family_tree.geektrust.Geektrust.construct_add_child_method_call', return_value = 'self.family_tree.add_child("Member", "Male", "Mother")')
-    @patch('family_tree.geektrust.Geektrust.construct_add_spouse_method_call', return_value = 'self.family_tree.add_spouse("Wife", "Female", "Spouse")')
-    @patch('family_tree.geektrust.Geektrust.construct_get_relationship_method_call', return_value= 'self.family.get_relationship("Member", "Brother_in_law")')
-    def test_translate(self, mock_construct_get_relationship_method_call, mock_construct_add_spouse_method_call, mock_construct_add_child_method_call ):
+
+    def test_translate(self):
         with patch('builtins.open', create = True) as mock_open:
             mock_open.return_value.__enter__.return_value.readlines.return_value = (
                 'ADD_CHILD Mother Member Male',
                 'ADD_SPOUSE Spouse Wife Female',
                 'GET_RELATIONSHIP Member Brother_in_law'
+                'GET_RELATIONSHIP Member Random'
             )
             result = self.geektrust_app.translate('dummy_file.txt')
             self.assertEqual(
